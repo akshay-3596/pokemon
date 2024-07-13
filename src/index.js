b402a500bc2d8d486ad70241ca24715ff0ddc48e
akshay-3596 <akshaysonawane5842@gmail.com>
2024-07-13 15:24:02 +0530

    new commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import Pokemon from './components/Pokemon';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
